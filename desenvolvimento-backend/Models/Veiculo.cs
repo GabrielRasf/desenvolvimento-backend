5e5ead27b77a8a975fc53a08d73793f44c5e0a97
Gabriel R <gabrielrasf@hotmail.com>
2025-05-13 16:01:01 -0300

    Configuração do BD e tabela veiculo

---
+++ desenvolvimento-backend/Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace desenvolvimento_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Informe o ano de fabricação")]
+        public int AnoFacricacao { get; set; }
+
+        [Required(ErrorMessage = "Informe o ano do modelo")]
+        public int AnoModelo { get; set; }
+
+    }
+}
