5efd1c5183b9c62c812608d576c79c177cf351e6
Gabriel R <gabrielrasf@hotmail.com>
2025-05-15 10:26:53 -0300

    CRUD veículo

--- desenvolvimento-backend/Migrations/20250513211724_UpdateVeiculoSchema.Designer.cs
+++ desenvolvimento-backend/Migrations/20250513211724_UpdateVeiculoSchema.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using desenvolvimento_backend.Models;
 
@@ -10,9 +11,11 @@ using desenvolvimento_backend.Models;
 namespace desenvolvimento_backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250513211724_UpdateVeiculoSchema")]
+    partial class UpdateVeiculoSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ desenvolvimento-backend/Migrations/20250513211724_UpdateVeiculoSchema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace desenvolvimento_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVeiculoSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AnoFacricacao",
+                table: "Veiculos",
+                newName: "AnoFabricacao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AnoFabricacao",
+                table: "Veiculos",
+                newName: "AnoFacricacao");
+        }
+    }
+}
